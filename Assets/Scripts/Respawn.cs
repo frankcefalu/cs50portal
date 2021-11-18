bed1ca63852f6176b39558222470c300624dbe85
Frank Cefalu <frankcefalu@gmail.com>
2021-11-17 22:18:41 -0500

    Commit

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    Vector3 Pos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Pos = transform.position;
+        if (Pos.y < -230)
+        {
+            transform.position = new Vector3(7.5f, 1.97f, 2.6f);
+        }
+    }
+}
---
+++ Assets/Scripts/Win.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Win : MonoBehaviour
+{
+    public GameObject message;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        message.SetActive(true);
+
+    }
+
+
+}
